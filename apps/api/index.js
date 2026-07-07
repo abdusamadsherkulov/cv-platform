5378edd7b028c2abca9b081fbde9411653cddf3d
Abdusamad Sherkulov <abdusamadsherkulov@gmail.com>
2026-07-07 20:19:14 +0500

    initial commit

---
+++ apps/api/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 4000;
+
+app.get('/health', (req, res) => {
+    res.json({status: 'ok'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
