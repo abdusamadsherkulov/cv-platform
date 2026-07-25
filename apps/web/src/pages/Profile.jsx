bb5b37d982d9feab958a0950a4aadbe380f5a9af
Abdusamad Sherkulov <abdusamadsherkulov@gmail.com>
2026-07-26 04:17:44 +0500

    profile page my projects section lengths adjusted

--- apps/web/src/pages/Profile.jsx
+++ apps/web/src/pages/Profile.jsx
@@ -252,11 +252,11 @@ function Profile() {
           <table className="table">
             <thead>
               <tr>
-                <th style={{ minWidth: '150px' }}>{t('projects.colName')}</th>
-                <th style={{ minWidth: '180px' }}>{t('projects.colPeriod')}</th>
-                <th style={{ minWidth: '150px' }}>{t('projects.colTags')}</th>
-                <th style={{ width: '100%' }}>{t('projects.colDescription')}</th>
-                <th></th>
+                <th style={{ minWidth: '150px' }} >{t('projects.colName')}</th>
+                <th style={{ minWidth: '180px' }} className='text-center'>{t('projects.colPeriod')}</th>
+                <th style={{ minWidth: '150px' }} className='text-center'>{t('projects.colTags')}</th>
+                <th style={{ width: '100%' }} className='text-center'>{t('projects.colDescription')}</th>
+                <th style={{ minWidth: '200px'}} ></th>
               </tr>
             </thead>
             <tbody>
@@ -438,12 +438,12 @@ function ProfileProjectRow({ project, onDelete, onSaved }) {
       ) : (
         <>
           <td style={{ minWidth: '150px' }}>{project.name}</td>
-          <td style={{ minWidth: '180px' }}>
+          <td style={{ minWidth: '180px' }} className='text-center'>
             {new Date(project.startDate).toLocaleDateString()} -{' '}
             {project.endDate ? new Date(project.endDate).toLocaleDateString() : t('projects.ongoing')}
           </td>
-          <td style={{ minWidth: '150px' }}>{project.tags.join(', ')}</td>
-          <td style={{ width: '100%' }}>{project.description}</td>
+          <td style={{ minWidth: '150px' }} className='text-center'>{project.tags.join(', ')}</td>
+          <td style={{ width: '100%' }} className='text-center'>{project.description}</td>
         </>
       )}
       <td className="d-flex gap-2">
